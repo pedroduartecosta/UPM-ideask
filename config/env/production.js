2b0f8096d599a04e89f47dabd332fa393138171e
Pedro Costa <pedrodc23@gmail.com>
2018-11-14 18:28:59 +0100

    fixed deploy

--- config/env/production.js
+++ config/env/production.js
@@ -336,7 +336,7 @@ module.exports = {
   * this, just try deploying without setting it and see if it works.)       *
   *                                                                         *
   ***************************************************************************/
-  port: 80,
+  // port: 80,
 
 
 
