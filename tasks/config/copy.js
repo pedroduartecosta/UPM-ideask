0aba38d910e76b6b107e881918fe14508868c02d
Pedro Costa <pedrodc23@gmail.com>
2018-11-15 23:35:15 +0100

    testing

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -25,7 +25,7 @@ module.exports = function(grunt) {
         expand: true,
         cwd: '.tmp/public',
         src: ['**/*'],
-        dest: 'www'
+        dest: '.www'
       }]
     },
     beforeLinkBuildProd: {
