055ad83e0dfc79ba11f41e4f32ae79428ac7d3e3
Christoph Schwägerl <cs@christophs-imac.speedport_w_724v_09011603_05_017>
2018-11-19 21:44:28 +0100

    created tests for Register action and User model

--- test/integration/models/User.test.js
+++ test/integration/models/User.test.js
@@ -1,25 +1,67 @@
 var util = require('util');
 
+var chai = require('chai')
+const chaiSubset = require('chai-subset');
+chai.use(chaiSubset);
+
+const expect = chai.expect
+
+const validUser = {
+  emailAddress: 'walter@white.com',
+  firstName: 'Walter',
+  lastName: 'White',
+  password: 'ww'
+}
+
+const invalidUser = {
+  e: 'walter@white.com',
+  f: 'Walter',
+  l: 'White',
+  p: 'ww'
+}
+
 describe('User (model)', function() {
 
-  /*describe('#findBestStudents()', function() {
-    it('should return 5 users', function (done) {
-      User.findBestStudents()
-      .then(function(bestStudents) {
+  this.beforeEach(async () => {
+    //clean database before testing
+    return await User.destroy({}).fetch()
+  })
 
-        if (bestStudents.length !== 5) {
-          return done(new Error(
-            'Should return exactly 5 students -- the students '+
-            'from our test fixtures who are considered the "best".  '+
-            'But instead, got: '+util.inspect(bestStudents, {depth:null})+''
-          ));
-        }//-•
+  this.afterAll(async () => {
+    //clean database after testing
+    return await User.destroy({}).fetch()
+  })
 
-        return done();
+  it('should be created successfully', async () => {
+    //act
+    const user = await User.create(validUser).fetch()
 
+    //assert
+    expect(user).to.not.equal(undefined)
+    expect(user).to.containSubset(validUser)
+  }),
+
+  it('should fail for invalid properties', (done) => {
+    User.create(invalidUser).then(user => {
+      console.log('user: ' + user)
+      return done(new Error('Did not fail'))
+    })
+    .catch((error) => {
+      return done()
+    })
+  }),
+
+  it('should fail for existing email', (done) => {
+    User.create(validUser).then(user => {
+      User.create(validUser).then(user => {
+        return done(new Error('Did not fail'))
       })
-      .catch(done);
-    });
-  });*/
-
+      .catch((error) => {
+        return done()
+      })      
+    })
+    .catch((error) => {
+      return done(new Error('Did not create first user'))
+    })
+  })
 });
