2ac3277f60e96528aa24b5400307a9be6a59029b
Christoph-Rgb <chris.schwaegerl@gmail.com>
2018-11-22 12:14:59 +0100

    created test cases for US Login

--- test/integration/controllers/EntranceController.test.js
+++ test/integration/controllers/EntranceController.test.js
@@ -12,26 +12,29 @@ const validUser = {
     password:'ww'
 }
 
-const invalidUser = {
+const userWithWrongProperties = {
     f:'Walter',
     l:'White',
     e:'walter@white.com',
     p:'ww'
 }
 
+const notExistingEmail = 'jessie@pinkman.com'
+const notMatchingPassword = 'jp'
+
 describe('EntranceController', function() {
 
-    describe('#register()', () => {
-
-        this.beforeEach(async () => {
-            //clean database before testing
-            return await User.destroy({}).fetch()
-          })
-        
-        this.afterAll(async () => {
+    this.beforeEach(async () => {
+        //clean database before testing
+        return await User.destroy({}).fetch()
+      })
+    
+    this.afterAll(async () => {
         //clean database after testing
         return await User.destroy({}).fetch()
-        })
+    })
+
+    describe('#register()', () => {
 
         it('should succeed for valid input', async () => {
             // act assert
@@ -56,10 +59,95 @@ describe('EntranceController', function() {
             // act assert
             return await supertest(sails.hooks.http.app)
                             .post('/api/v1/entrance/signup')
-                            .send(invalidUser)
+                            .send(userWithWrongProperties)
                             .expect(400)
         })
+    }),
 
+    describe('#login()', () => {
+
+        this.afterEach(async () => {
+            //clean database after testing
+            return await User.destroy({}).fetch()
+        })
+
+        it('should succeed for valid input', async () => {
+            //arrange
+            //create a valid record in the database
+            const hashedPassword = await sails.helpers.passwords.hashPassword(validUser.password)
+            const userToCreate = {
+                emailAddress: validUser.emailAddress,
+                password: hashedPassword,
+                firstName: validUser.firstName,
+                lastName: validUser.lastName
+            }
+
+            const user = await User.create(userToCreate).fetch()
+            if (!user) {
+                throw new Error('Creating user failed')
+            }
+
+            //act assert
+            return await supertest(sails.hooks.http.app)
+                            .put('/api/v1/entrance/login')
+                            .send({
+                                emailAddress: validUser.emailAddress,
+                                password: validUser.password
+                            })
+                            .expect(200)
+        }),
+
+        it('should fail for not existing user', async () => {
+            //arrange
+            //create a valid record in the database
+            const hashedPassword = await sails.helpers.passwords.hashPassword(validUser.password)
+            const userToCreate = {
+                emailAddress: validUser.emailAddress,
+                password: hashedPassword,
+                firstName: validUser.firstName,
+                lastName: validUser.lastName
+            }
+
+            const user = await User.create(userToCreate).fetch()
+            if (!user) {
+                throw new Error('Creating user failed')
+            }
+
+            //act assert
+            return await supertest(sails.hooks.http.app)
+                            .put('/api/v1/entrance/login')
+                            .send({
+                                emailAddress: notExistingEmail,
+                                password: validUser.password
+                            })
+                            .expect(401)
+        }),
+
+        it('should fail for bad password/username combination', async() => {
+            //arrange
+            //create a valid record in the database
+            const hashedPassword = await sails.helpers.passwords.hashPassword(validUser.password)
+            const userToCreate = {
+                emailAddress: validUser.emailAddress,
+                password: hashedPassword,
+                firstName: validUser.firstName,
+                lastName: validUser.lastName
+            }
+
+            const user = await User.create(userToCreate).fetch()
+            if (!user) {
+                throw new Error('Creating user failed')
+            }
+
+            //act assert
+            return await supertest(sails.hooks.http.app)
+                            .put('/api/v1/entrance/login')
+                            .send({
+                                emailAddress: validUser.emailAddress,
+                                password: notMatchingPassword
+                            })
+                            .expect(401)
+        })
 
     })
 
