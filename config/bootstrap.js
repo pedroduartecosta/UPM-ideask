fa7e116edea4e5485b8207c07ae5fee727579d64
Pedro Costa <pedrodc23@gmail.com>
2018-11-26 10:56:24 +0100

    Removing old bootstrap data

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -59,9 +59,9 @@ module.exports.bootstrap = async function() {
   }//∞
 
   // By convention, this is a good place to set up fake data during development.
-  await User.createEach([
-    { emailAddress: 'admin@example.com', fullName: 'Ryan Dahl', isSuperAdmin: true, password: await sails.helpers.passwords.hashPassword('abc123') },
-  ]);
+  //await User.createEach([
+  //  { emailAddress: 'admin@example.com', fullName: 'Ryan Dahl', isSuperAdmin: true, password: await sails.helpers.passwords.hashPassword('abc123') },
+  //]);
 
   // Save new bootstrap version
   await sails.helpers.fs.writeJson.with({
