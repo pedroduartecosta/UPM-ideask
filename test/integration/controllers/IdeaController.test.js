35cfadd41d178a3ae1cfeca1376cd6d32e3ca188
Christoph-Rgb <chris.schwaegerl@gmail.com>
2018-11-27 21:14:28 +0100

    fixed login problems for tests

--- test/integration/controllers/IdeaController.test.js
+++ test/integration/controllers/IdeaController.test.js
@@ -33,48 +33,48 @@ const invalidIdea = {
 
 describe('IdeaController', function () {
 
+  var agent = undefined
+  var user = undefined
+
+  this.beforeAll(async () => {
+    //clean database before testing
+
+    await Idea.destroy({}).fetch()
+    await User.destroy({}).fetch()
+
+    const hashedPassword = await sails.helpers.passwords.hashPassword(validUser.password)
+
+    user = await User.create({ 
+      emailAddress: validUser.emailAddress,
+      password: hashedPassword,
+      firstName: validUser.firstName,
+      lastName: validUser.lastName
+      }).fetch()
+
+    agent = supertest.agent(sails.hooks.http.app)
+    await agent.put('/api/v1/entrance/login')
+                      .send(
+                        {
+                          emailAddress: validUser.emailAddress,
+                          password: validUser.password
+                        }
+                      )
+                      .expect(200)
+  })
+
+
+  this.afterAll(async () => {
+    //clean database after testing
+    await User.destroy({}).fetch()
+    return await Idea.destroy({}).fetch()
+  })
+
   describe('Create idea', () => {
 
-    this.beforeAll(async () => {
-      //clean database before testing
-    })
-
-    this.afterEach(async () => {
-      //clean database after testing
-      return await User.destroy({}).fetch()
-    })
-
-    this.afterAll(async () => {
-      //clean database after testing
-      await User.destroy({}).fetch()
-      return await Idea.destroy({}).fetch()
-    })
-
     it('should succeed for valid input', async () => {
-      //arrange
-      //create a valid record in the database
-      const hashedPassword = await sails.helpers.passwords.hashPassword(validUser.password)
-      const userToCreate = {
-          emailAddress: validUser.emailAddress,
-          password: hashedPassword,
-          firstName: validUser.firstName,
-          lastName: validUser.lastName
-      }
-
-      const user = await User.create(userToCreate).fetch()
-      if (!user) {
-          throw new Error('Creating user failed')
-      }
-      await supertest(sails.hooks.http.app)
-              .put('/api/v1/entrance/login')
-              .send({
-                  emailAddress: validUser.emailAddress,
-                  password: validUser.password
-              })
-              .expect(200)
 
       //act assert
-      return await supertest(sails.hooks.http.app)
+      return await agent
           .post('/api/v1/ideas/new-idea')
           .send(validIdea)
           .expect(200)
@@ -82,7 +82,7 @@ describe('IdeaController', function () {
 
     it('should fail for invalid input', async () => {
       // act assert
-      return await supertest(sails.hooks.http.app)
+      return await agent
         .post('/api/v1/ideas/new-idea')
         .send(invalidIdea)
         .expect(400)
@@ -93,24 +93,14 @@ describe('IdeaController', function () {
 
   describe('List ideas for user', () => {
 
-    this.user = undefined
-
-    this.beforeEach(async () => {
-      const hashedPassword = await sails.helpers.passwords.hashPassword(validUser.password)
-
-      this.user = await User.create({ 
-        emailAddress: validUser.emailAddress,
-        password: hashedPassword,
-        firstName: validUser.firstName,
-        lastName: validUser.lastName
-        }).fetch()
+    this.beforeAll(async () => {
 
       await Idea.create({
         title: validIdea.title,
         subtitle: validIdea.subtitle,
         description: validIdea.description,
         notes: validIdea.notes,
-        owner: this.user.id
+        owner: user.id
       })
 
       return await Idea.create({
@@ -118,43 +108,27 @@ describe('IdeaController', function () {
         subtitle: secondValidIdea.subtitle,
         description: secondValidIdea.description,
         notes: secondValidIdea.notes,
-        owner: this.user.id
+        owner: user.id
       })
+    }),
 
-    })
 
-    this.afterEach(async () => {
-      //clean database after testing
-      await Idea.destroy({}).fetch()
-      return await User.destroy({}).fetch()
-    })
-
-    
     it('should succeed for logged in user', async () => {
-      var request = require('superagent');
-      var user1 = request.agent(sails.hooks.http.app);
-      await user1
-        .put('/api/v1/entrance/login')
-        .send({ emailAddress: this.user.emailAddress, password: this.user.password });
 
-      return await user1
+      return await agent
           .get('/users/ideas')
           .expect(200)
 
     }),
 
-    it('should succeed for specified user', async () => {
-      var request = require('superagent');
-      var user1 = request.agent(sails.hooks.http.app);
-      await user1
-        .put('/api/v1/entrance/login')
-        .send({ emailAddress: this.user.emailAddress, password: this.user.password });
+    it('should succeed for given user id', async () => {
 
-      return await user1
-          .get('/users/' + this.user.id + '/ideas')
-          .expect(200)
+      return await agent
+            .get(`/users/${user.id}/ideas`)
+            .expect(200)
 
     })
+
 
   })
 
