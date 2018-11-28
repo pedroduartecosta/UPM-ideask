a670d0bc644411f7179ecfdae1597b73cb2ca4db
Pedro Costa <pedrodc23@gmail.com>
2018-11-28 14:39:15 +0100

    Deleting idea

--- api/models/User.js
+++ api/models/User.js
@@ -11,7 +11,11 @@ module.exports = {
     //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
     //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
     //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
-
+    id: { 
+      type: 'string', 
+      columnName: '_id' 
+    },
+    
     emailAddress: {
       type: 'string',
       required: true,
